56edddae2d1e306fa825747a2b4f7787b14712b1
yoshihara <yoshihara@users.noreply.github.com>
2018-01-31 00:53:03 +0900

    Exersise

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,32 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "something",
+      entered: "entered"
+    };
+  }
+
+  onChange(e) {
+    this.setState({message: e.target.value});
+  }
+
+  onKeyDown(e) {
+    if (e.keyCode == 13) { // 13: Enter
+      this.setState({entered: this.state.message})
+    }
+  }
+
   render () {
-    return <p> Hello React!</p>;
+    return (
+      <div>
+        <input type="text" onChange={ this.onChange.bind(this) } onKeyDown={ this.onKeyDown.bind(this) }/>
+        <p>{ this.state.message }</p>
+        <p>{ this.state.entered }</p>
+      </div>
+    );
   }
 }
 
