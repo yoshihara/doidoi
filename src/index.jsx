1036a68f8c384efcc03f03a9803744ab3e72bd03
yoshihara <yoshihara@users.noreply.github.com>
2018-02-22 22:10:21 +0900

    lint

--- src/index.jsx
+++ src/index.jsx
@@ -1,54 +1,74 @@
-'use strict';
+"use strict";
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import {render} from 'react-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { render } from "react-dom";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {todos: [{done: true, text: 'hoge'}, {done: false, text: 'fuga'}]};
+    this.state = {
+      todos: [{ done: true, text: "hoge" }, { done: false, text: "fuga" }]
+    };
   }
 
   render() {
-    return (
-      <Todos todos={this.state.todos}></Todos>
-    )
+    return <Todos todos={this.state.todos} />;
   }
 }
 
 class Todos extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {todos: this.props.todos};
+    this.state = { todos: this.props.todos };
   }
 
   render() {
-    return(
+    return (
       <ul>
         {this.state.todos.map((todo, i) => {
-          return(
+          return (
             <li key={i}>
-              <input type='number' className='orders' />
-              <input type='checkbox' defaultChecked={todo.done} className='done'/>
-              <input type='text' defaultValue={todo.text} className='text' />
+              <input type="number" className="orders" />
+              <input
+                type="checkbox"
+                defaultChecked={todo.done}
+                className="done"
+              />
+              <input type="text" defaultValue={todo.text} className="text" />
             </li>
-          )
+          );
         })}
       </ul>
-    )
+    );
   }
 }
+
 Todos.propTypes = {
-  todos: PropTypes.arrayOf(function(propValue, key, componentName, location, propFullName) {
+  todos: PropTypes.arrayOf(function(
+    propValue,
+    key,
+    componentName,
+    location,
+    propFullName
+  ) {
     let value = propValue[key];
-    if (typeof value.done !== 'boolean') {
-      return new Error(`${componentName}.props.${propFullName}.done should be boolean, but ${value.done}`);
-    } else if (typeof value.text !== 'string') {
-      return new Error(`${componentName}.props.${propFullName}.text should be string, but ${value.text}`);
+    if (typeof value.done !== "boolean") {
+      return new Error(
+        `${componentName}.props.${propFullName}.done should be boolean, but ${
+          value.done
+        }`
+      );
+    } else if (typeof value.text !== "string") {
+      return new Error(
+        `${componentName}.props.${propFullName}.text should be string, but ${
+          value.text
+        }`
+      );
     } else {
       return true;
     }
   })
-}
-render(<App/>, document.getElementById('app'));
+};
+
+render(<App />, document.getElementById("app"));
